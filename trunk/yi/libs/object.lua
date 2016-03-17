355aeadc8674ccb9905486600e40125ae78bca0f
jjdjr <jjdjr@e5654f7a-a930-4ce2-a674-dcd4db6ea1e1>
2016-03-17 07:47:53 +0000

    - 增加 object.lua 文件。

---
+++ trunk/yi/libs/object.lua
@@ -0,0 +1,43 @@
+local meta = {
+	name = "object"
+}
+
+function meta:extend(class)
+	class = class or {}
+	setmetatable(class, self)
+	self.__index = self
+
+	return class
+end
+
+meta.new = meta.extend
+
+return meta
+
+--[[ 
+--sample:
+
+local base = meta:extend()
+
+base.key1 = "base.key1"
+base.key2 = "base.key2"
+
+local a = base:extend({})
+local b = base:extend({})
+local c = base:extend({})
+
+a.key1 = "a.key1"
+b.key1 = "b.key1"
+c.key1 = "c.key1"
+
+print("base.key1: " .. base["key1"])
+print("base.key2: " .. base["key2"])
+
+print("a.key1: " .. a["key1"])
+print("b.key1: " .. b["key1"])
+print("c.key1: " .. c["key1"])
+
+print("a.key2: " .. a["key2"])
+print("b.key2: " .. b["key2"])
+print("c.key2: " .. c["key2"])
+--]]
