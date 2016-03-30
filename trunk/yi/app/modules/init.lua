56c13a8ec585d3839adbda4dd8703675f6925af2
jjdjr <jjdjr@e5654f7a-a930-4ce2-a674-dcd4db6ea1e1>
2016-03-30 16:18:53 +0000

    增加 app.view app.entity app.table 等方法

--- trunk/yi/app/modules/init.lua
+++ trunk/yi/app/modules/init.lua
@@ -17,4 +17,16 @@ function app.response(name)
     return Yi.use(name .. '.response')
 end
 
+function app.view(name, view)
+    return Yi.view(name .. '.view.' .. view)
+end
+
+function app.entity(name)
+    return Yi.load('data.entity.' .. name)
+end
+
+function app.table(name)
+    return Yi.load('data.config.' .. name)
+end
+
 Yi.facade:registerModules(modules_init_)
