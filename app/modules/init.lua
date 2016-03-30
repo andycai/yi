cc3980b0878a01eb75d467330f595a840f72858d
jjdjr <jjdjr@e5654f7a-a930-4ce2-a674-dcd4db6ea1e1>
2016-03-30 16:04:26 +0000

    增加 app.model app.service app.response 等方法

--- app/modules/init.lua
+++ app/modules/init.lua
@@ -5,4 +5,16 @@ local modules_init_ = {
 	nil,
 }
 
+function app.model(name)
+    return Yi.use(name .. '.model')
+end
+
+function app.service(name)
+    return Yi.use(name .. '.service')
+end
+
+function app.response(name)
+    return Yi.use(name .. '.response')
+end
+
 Yi.facade:registerModules(modules_init_)
