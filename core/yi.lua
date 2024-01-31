cbe5aae7b06ddd7fcd8df444a08b3087482ffae3
Andy Cai <huayicai@gmail.com>
2024-01-31 15:27:10 +0800

    Add Cell super class for view, model, service and handler.

--- core/yi.lua
+++ core/yi.lua
@@ -1,6 +1,6 @@
 local format = string.format
 
-Yi = {
+local yi_ = {
 	_lang = 'zh_cn',
 	_log = false,
 	_isInit = false,
@@ -13,22 +13,22 @@ local Facade = {
 	_actors = {}
 }
 
-Yi.facade = Facade
+yi_.facade = Facade
 
 local class = require('libs.middleclass')
 
-function Yi.use(path)
+function yi_.use(path)
 	return require(APPPATH .. 'modules.' .. path)
 end
 
-function Yi.message(file, path)
+function yi_.message(file, path)
 	local messages = require(APPPATH .. 'messages.' .. path)
 	return messages[path]
 end
 
-function Yi.mod(moduleName)
-	if Yi._modules[moduleName] then
-		return Yi._modules[moduleName]
+function yi_.mod(moduleName)
+	if yi_._modules[moduleName] then
+		return yi_._modules[moduleName]
 	end
 
 	local obj ={
@@ -36,75 +36,75 @@ function Yi.mod(moduleName)
 	}
 
 	obj.newObj = function(path)
-		local cls = Yi.use(format('%s.class.%s', moduleName, path))
+		local cls = yi_.use(format('%s.class.%s', moduleName, path))
 		return cls:new()
 	end
 
 	obj.newWidget = function(path)
-		local cls = Yi.use(format('%s.widget.%s', moduleName, path))
+		local cls = yi_.use(format('%s.widget.%s', moduleName, path))
 		return cls:new()
 	end
 	obj.loadWidget = function(path)
-		return Yi.use(format('%s.widget.%s', moduleName, path))
+		return yi_.use(format('%s.widget.%s', moduleName, path))
 	end
 
 	local mt = {}
 	setmetatable(obj, mt)
 	mt.__index = function(mtable, key)
-		return Yi.use(format('%s.%s', mtable.moduleName, key))
+		return yi_.use(format('%s.%s', mtable.moduleName, key))
 	end
-	Yi._modules[moduleName] = obj
+	yi_._modules[moduleName] = obj
 
 	return obj
 end
 
-Yi.go = {}   -- go to module
-Yi.go.send = function(...)
+yi_.go = {}   -- go to module
+yi_.go.send = function(...)
 	Facade:send(...)
 end
 
 local modulemt_ = {}
-setmetatable(Yi.go, modulemt_)
+setmetatable(yi_.go, modulemt_)
 modulemt_.__index = function(_, key)
-	assert(Yi._moduleNames[key], "module doesn't exists: " .. key)
-	return Yi.mod(Yi._moduleNames[key])
+	assert(yi_._moduleNames[key], "module doesn't exists: " .. key)
+	return yi_.mod(yi_._moduleNames[key])
 end
 
-function Yi.reload(path)
+function yi_.reload(path)
 	package.loaded[path] = nil
 end
 
-function Yi.init(settings)
-	if Yi._isInit then
+function yi_.init(settings)
+	if yi_._isInit then
 		return
 	end
 
-	Yi._isInit = true
+	yi_._isInit = true
 
 	if settings.log then
-		Yi._log = settings.log
+		yi_._log = settings.log
 	end
 
 	if settings.lang then
-		Yi._lang = settings.lang
+		yi_._lang = settings.lang
 	end
 end
 
-function Yi.lang()
-	return Yi._lang
+function yi_.lang()
+	return yi_._lang
 end
 
 --[==[
 Event
 --]==]
 
-Yi.Event = {}
-Yi.Event.send = function(...)
+yi_.Event = {}
+yi_.Event.send = function(...)
 	Facade:send(...)
 end
 
 local e_mt = {}
-setmetatable(Yi.Event, e_mt)
+setmetatable(yi_.Event, e_mt)
 e_mt.__index = function(table, key)
 	return 'Event.' .. key
 end
@@ -149,10 +149,10 @@ function Facade:registerActor(moduleName)
 
 	local actor_ = self._actors[moduleName]
 	assert(actor_ == nil, "module name repetition:" .. moduleName)
-	assert(Yi._moduleNames[unique_name] == nil, "unique name repetition:" .. unique_name)
-	Yi._moduleNames[unique_name] = moduleName
+	assert(yi_._moduleNames[unique_name] == nil, "unique name repetition:" .. unique_name)
+	yi_._moduleNames[unique_name] = moduleName
 
-	local Actor_ = Yi.use(moduleName..'.actor')
+	local Actor_ = yi_.use(moduleName..'.actor')
 	actor_ = Actor_:new(moduleName)
 	self._actors[moduleName] = actor_
 	actor_:onRegister()
@@ -190,7 +190,7 @@ class Actor
 --]==]
 local Actor = class("Actor")
 
-Yi.Actor = Actor
+yi_.Actor = Actor
 
 Actor._handlers = {}
 Actor.static.instance_ = nil
@@ -226,21 +226,21 @@ end
 
 function Actor:model(moduleName)
 	moduleName = moduleName or self._name
-	return Yi.mod(moduleName).model
+	return yi_.mod(moduleName).model
 end
 
 function Actor:service(moduleName)
 	moduleName = moduleName or self._name
-	return Yi.mod(moduleName).service
+	return yi_.mod(moduleName).service
 end
 
 function Actor:view()
-	return Yi.mod(self._name).view
+	return yi_.mod(self._name).view
 end
 
 function Actor:request(action, param)
-	if IsFunction(Yi.request) then
-		Yi.request(action, param)
+	if IsFunction(yi_.request) then
+		yi_.request(action, param)
 	end
 end
 
@@ -257,7 +257,7 @@ function Actor:on(action, param)
 	if IsFunction(on_) then
 		on_resp_ = true
 	else
-		local resp_ = Yi.use(self._name .. '.response')
+		local resp_ = yi_.use(self._name .. '.response')
 		assert(resp_, self._name .. '.response is nil')
 		local act_ = string.explode(action, ".")
 		local method_ = act_[2]
@@ -273,4 +273,60 @@ function Actor:on(action, param)
 	end
 end
 
-return Yi
+--[==[
+object 
+--]==]
+local Object = require('libs.object')
+local cell = Object:extend()
+
+yi_.Cell = cell
+
+function cell:model(moduleName)
+	moduleName = moduleName or self.name
+	assert(moduleName ~= nil, 'moduleName is empty calling model')
+	return yi_.mod(moduleName).model
+end
+
+function cell:service(moduleName)
+	moduleName = moduleName or self.name
+	assert(moduleName ~= nil, 'moduleName is empty calling service')
+	return yi_.mod(moduleName).service
+end
+
+function cell:handler()
+	local moduleName = self.name
+	assert(moduleName ~= nil, 'moduleName is empty calling handler')
+	return yi_.mod(moduleName).handler
+end
+
+function cell:newObj(path, moduleName)
+	moduleName = moduleName or self.name
+	assert(moduleName ~= nil, 'moduleName is empty calling newObj')
+	return yi_.mod(moduleName).newObj(path)
+end
+
+function cell:newWidget(path, moduleName)
+	moduleName = moduleName or self.name
+	assert(moduleName ~= nil, 'moduleName is empty calling newWidget')
+	return yi_.mod(moduleName).newWidget(path)
+end
+
+function cell:loadWidget(path, moduleName)
+	moduleName = moduleName or self.name
+	assert(moduleName ~= nil, 'moduleName is empty calling loadWidget')
+	return yi_.mod(moduleName).loadWidget(path)
+end
+
+-- function cell:view()
+-- 	local moduleName = self.name
+-- 	assert(moduleName ~= nil, 'moduleName is empty calling view')
+-- 	return Yi.mod(moduleName).view
+-- end
+
+-- 遵守开发约定
+-- 1. 模块之间必须通过事件来做交互，主要是界面的处理
+-- 2. 只有模块中的 Actor 层才能调用 view 层的接口，其他层如 model、service、handler 等都不能
+-- 3. 模块之间可以互相调用之间的 model 层和 service 层的公开接口
+-- 4. 模块 view 层，没有能力直接调用其他
+
+return yi_
